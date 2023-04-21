440b32ff783371449a435e42af95cedf33e19ffb
Hunter Jordan III <111809137+hjordanv3@users.noreply.github.com>
2023-04-21 17:35:07 -0400

    complete

--- bash.js
+++ bash.js
@@ -1,11 +1,21 @@
 //Output a prompt
 process.stdout.write('prompt > ');
-
+  
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim(); //remove the newline
+  const input = data.toString().trim(); //remove the newline
+  const [cmd, fileName] = input.split(' '); // split input into command and filename
   const pwd = require('./pwd');
-  process.stdout.write('You typed: ' + cmd);
+  const ls = require('./ls');
+  const cat = require('./cat')
+
+  process.stdout.write('You typed: ' + input);
   process.stdout.write('\nprompt > ');
   pwd(cmd);
+  if (cmd === "ls") {
+    ls()
+  }
+  if (cmd === "cat") {
+    cat(fileName) // pass filename to cat function
+  }
 });
